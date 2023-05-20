764e0cebd256b7f46db829aeca0c1bec2cef1893
BuildTools <unconfigured@null.spigotmc.org>
2023-05-20 15:02:29 +0200

    Added tasks styles

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ export default function App() {
         <Text style={styles.sectionTitle}>Tytuł taska</Text>
 
         <View style={styles.items}>
-          <Task></Task>
-          <Task></Task>
-          <Task></Task>
+          <Task text="Task 1"></Task>
+          <Task text="Task 2"></Task>
+          <Task text="Task 3"></Task>
         </View>
       </View>
     </View>
