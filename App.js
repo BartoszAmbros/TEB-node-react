0b06628a5511ff1e6a91aaacfe5e257614350b82
BuildTools <unconfigured@null.spigotmc.org>
2023-05-20 15:53:42 +0200

    Added base structure for adding tasks, mobile keyboard

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity, ScrollView } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const handleTask = function () { };
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
@@ -14,6 +15,14 @@ export default function App() {
           <Task text="Task 3"></Task>
         </View>
       </View>
+      <KeyboardAvoidingView style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a task'} />
+        <TouchableOpacity onPress={function () { handleTask() }}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -33,5 +42,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
-  }
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    padding: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
